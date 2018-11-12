7cc2f36a8b795a96f63bcaed883fffd5cb3cae30
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-12 08:22:19 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,25 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_reader :text
+
+  def initialize(text)
+    @text = text.downcase
+  end
+
+  def count_of_words
+    words = text.split(" ")
+    words.count 
+  end 
+
+  def count_of_vowels
+    text.scan(/[aeoui]/).count
+  end 
+
+  def count_of_consonants
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count 
+  end
+
+  def most_used_letter
+    s1 = text.gsub(/[^a-z]/, '')  #gets rid of spaces
+  end 
+end 
